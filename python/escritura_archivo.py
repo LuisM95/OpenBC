bfa773087775b14488a0474849283f958526a62d
Luis Martel <martel19.lm@gmail.com>
2022-10-27 23:03:17 +0200

    Actualizando codespaces

--- python/escritura_archivo.py
+++ python/escritura_archivo.py
@@ -3,18 +3,27 @@ def escribe(fichero, datos):
     f = open(fichero, 'w')
 
     for linea in datos:
-        if linea.endswith('\n') == False:
-        	linea = linea + '\n'
-        
-        
+        if not linea.endswith('\n'):
+            linea = linea + '\n'
         f.write(linea)
 
     f.close()
 
-lista =[
-    'Escribiendo un fichero con python', 
-    'con El curso de openbootcamp', 
-    'desde codespaces'
+
+lista = [
+    'aprendiendo a escribir',
+    'un archivo en python con',
+    'openbootcamp y codespaces'
 ]
 
-escribe('miarchivo.txt', lista)
+escribe('python/miarchivo.txt', lista)
+
+texto = input("Ingresa el texto a agregar:\n")
+def agregarTexto( fichero, datos):
+    f = open(fichero, 'a')
+    if texto != '':
+        f.write(texto)
+    else:
+        print("No hay nada que agregar")
+
+agregarTexto('python/miarchivo.txt', texto)
